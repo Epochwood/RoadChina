15e37a1403c153a475a10ff57b4b0d1076c8b90d
Heliecp <111218716+Heliecp@users.noreply.github.com>
2022-12-31 20:58:54 +0800

    重置贴图和模型

--- src/main/java/heliecp/roadchina/Block/ArrowA.java
+++ src/main/java/heliecp/roadchina/Block/ArrowA.java
@@ -1,23 +1,24 @@
 package heliecp.roadchina.Block;
 
+import heliecp.roadchina.Item.ItemRegistry;
 import heliecp.roadchina.Item.Wrench;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockRenderType;
-import net.minecraft.block.StairsBlock;
-import net.minecraft.block.BlockState;
+import net.minecraft.block.*;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.RenderTypeLookup;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.item.ItemStack;
+import net.minecraft.state.EnumProperty;
 import net.minecraft.state.StateContainer;
 import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.state.properties.SlabType;
 import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
@@ -27,6 +28,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 public class ArrowA extends Block
 {
     public static final Block block = null;
+
     public ArrowA()
     {
         super(Block.Properties.of(Material.STONE).harvestLevel(10));
@@ -36,7 +38,7 @@ public class ArrowA extends Block
     @Override
     public VoxelShape getCollisionShape(BlockState state, IBlockReader world, BlockPos pos, ISelectionContext context)
     {
-        return Block.box(0.0D,0.0D,0.0D,0.0D,0.0D,0.0D);
+        return VoxelShapes.empty();
     }
 
     @Override
@@ -70,26 +72,6 @@ public class ArrowA extends Block
         builder.add(BlockStateProperties.FACING);
     }
 
-    @Override
-    public boolean canBeReplaced(BlockState blockState, BlockItemUseContext context) {
-        ItemStack itemstack = context.getItemInHand();
-        if (itemstack.getItem() == this.asItem()) {
-            if (context.replacingClickedOnBlock()) {
-                Direction direction = context.getClickedFace();
-                if (blockState.getBlock() == BlockRegistry.whiteLine1.get()) {
-                    return direction == Direction.UP || direction.getAxis().isHorizontal();
-                }
-            }
-            else {
-                return true;
-            }
-        }
-        else {
-            return false;
-        }
-        return false;
-    }
-
 
     @Override
     public ActionResultType use(BlockState blockState, World worldIn, BlockPos pos, PlayerEntity playerIn, Hand handIn, BlockRayTraceResult blockRayTraceResult)
@@ -99,8 +81,7 @@ public class ArrowA extends Block
             return ActionResultType.SUCCESS;
         }
 
-        if(playerIn.getMainHandItem().getItem() instanceof Wrench)
-        {
+        if(playerIn.getMainHandItem().getItem() instanceof Wrench) {
             Direction direction = blockState.getValue(BlockStateProperties.FACING);
             worldIn.setBlockAndUpdate(pos, blockState.setValue(BlockStateProperties.FACING, direction.getClockWise()));
             return ActionResultType.SUCCESS;
